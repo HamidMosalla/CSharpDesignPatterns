6e3ecf617996c25c94e81c127f8ad29decad9c27
Hamid Mosalla <Xellarix@gmail.com>
2017-10-28 15:32:46 +0330

    add more detail to bridge pattern main method

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -8,6 +8,17 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
+
+            /*
+             * https://en.wikipedia.org/wiki/Bridge_pattern
+             What problems can the Bridge design pattern solve?
+             An abstraction and its implementation should be defined and extended independently from each other.
+             A compile-time binding between an abstraction and its implementation should be avoided so that an implementation can be selected at run-time.
+
+            When using subclassing, different subclasses implement an abstract class in different ways.
+            But an implementation is bound to the abstraction at compile-time and can't be changed at run-time.
+            */
+
             var clientRepository = new ClientRepository();
             var clientDataObject = new ClientDataObject();
 
@@ -29,6 +40,7 @@ namespace Bridge
             productRepository.CopyObject(productDataObject);
             productRepository.RemoveObject(productDataObject);
             productRepository.SaveChanges();
+
 
 
 
