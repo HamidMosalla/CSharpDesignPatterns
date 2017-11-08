8b4f5885f558b10a0ca194e61d3dbf5ad7b47aaf
Hamid Mosalla <Xellarix@gmail.com>
2017-11-08 14:06:03 +0330

    add more detail about observer pattern to main method

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -11,6 +11,22 @@ namespace Observer
     {
         static void Main(string[] args)
         {
+            /*
+             * More info: https://en.wikipedia.org/wiki/Observer_pattern
+             What problems can the Observer design pattern solve?
+
+              A one-to-many dependency between objects should be defined without making the objects tightly coupled.
+              It should be ensured that when one object changes state an open-ended number of dependent objects are updated automatically.
+              It should be possible that one object can notify an open-ended number of other objects.
+
+            Defining a one-to-many dependency between objects by defining one object (subject)
+            that updates the state of dependent objects directly is inflexible because it commits (tightly couples) the subject to particular dependent objects.
+
+            Tightly coupled objects are hard to implement, change, test,
+            and reuse because they refer to and know about (how to update) many different objects with different interfaces.
+            */
+
+
             #region Observer pattern
 
             // Configure Observer pattern
