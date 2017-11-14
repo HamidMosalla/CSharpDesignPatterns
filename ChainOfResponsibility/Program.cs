c91c17860e88f5b7f760a928789a0df473838ddb
Hamid Mosalla <Xellarix@gmail.com>
2017-11-14 08:28:59 +0330

    add info for chain of responsibility pattern

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -8,6 +8,19 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
+
+            /*
+             * More info: https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
+             * http://www.dofactory.com/net/chain-of-responsibility-design-pattern
+            What problems can the Chain of Responsibility design pattern solve?
+               Coupling the sender of a request to its receiver should be avoided.
+               It should be possible that more than one receiver can handle a request.
+
+            Implementing a request directly within the class that sends the request is inflexible because it couples the class to a particular receiver and makes it impossible
+            to support multiple receivers.
+            */
+
+
             // Setup Chain of Responsibility
             Handler h1 = new ConcreteHandler1();
             Handler h2 = new ConcreteHandler2();
---
+++ Composite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
