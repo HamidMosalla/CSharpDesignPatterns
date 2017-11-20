d66a93e519065606bf92ac8d98ec370a935d8347
Hamid Mosalla <Xellarix@gmail.com>
2017-11-20 14:48:05 +0330

    add decorator design pattern

---
+++ Decorator/Component/Coffee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Component
+{
+    public interface ICoffee
+    {
+         double GetCost();
+         string GetIngredients();
+    }
+}
---
+++ Decorator/Component/SimpleCoffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Component
+{
+    public class SimpleCoffee : ICoffee
+    {
+        public double GetCost()
+        {
+            return 1;
+        }
+
+        public string GetIngredients()
+        {
+            return "Coffee";
+        }
+    }
+}
