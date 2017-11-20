9720dd0f75083b0785d4407336f7336cc49f726a
Hamid Mosalla <Xellarix@gmail.com>
2017-11-20 15:31:19 +0330

    add info to decorator main

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -8,6 +8,16 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
+            /*
+             * More info: https://en.wikipedia.org/wiki/Decorator_pattern
+                 What problems can the Decorator design pattern solve?
+                    Responsibilities should be added to (and removed from) an object dynamically at run-time.
+                    A flexible alternative to subclassing for extending functionality should be provided.
+
+                When using subclassing, different subclasses extend a class in different ways.
+                But an extension is bound to the class at compile-time and can't be changed at run-time.
+            */
+
             void PrintInfo(ICoffee coffee) => Console.WriteLine("Cost: " + coffee.GetCost() + "; Ingredients: " + coffee.GetIngredients());
 
             ICoffee c = new SimpleCoffee();
