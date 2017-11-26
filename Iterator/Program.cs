96b74405a5f30c1e8a6ca28cebd8487bd5bee944
Hamid Mosalla <Xellarix@gmail.com>
2017-11-26 04:53:41 +0330

    add info and source of examples to the iterator main

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -7,6 +7,19 @@ namespace Iterator
     {
         static void Main(string[] args)
         {
+
+            /*
+             * More info: https://en.wikipedia.org/wiki/Iterator_pattern
+             * http://www.dofactory.com/net/iterator-design-pattern
+         What problems can the Iterator design pattern solve?
+            The elements of an aggregate object should be accessed and traversed without exposing its representation (data structures).
+            New traversal operations should be defined for an aggregate object without changing its interface.
+
+            Defining access and traversal operations in the aggregate interface is inflexible
+            because it commits the aggregate to particular access and traversal operations
+            and makes it impossible to add new operations later without having to change the aggregate interface.
+           */
+
             var a = new ConcreteAggregate
             {
                 [0] = "Item A",
