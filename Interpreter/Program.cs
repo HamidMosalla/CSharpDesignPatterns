602d4a189ddb28b143e651facee2e7b2cae29a4e
Hamid Mosalla <Xellarix@gmail.com>
2017-11-29 11:03:27 +0330

    add info to interpreter man

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -9,8 +9,21 @@ namespace Interpreter
     {
         static void Main(string[] args)
         {
+            /*
+             * more info: https://sourcemaking.com/design_patterns/interpreter
+             * https://www.codeproject.com/Articles/186183/Interpreter-Design-Pattern
+            Intent
+            Given a language, define a representation for its grammar along with an interpreter that uses the representation to interpret sentences in the language.
+            Map a domain to a language, the language to a grammar, and the grammar to a hierarchical object-oriented design.
+
+            Problem
+            A class of problems occurs repeatedly in a well-defined and well-understood domain.
+            If the domain were characterized with a "language",
+            then problems could be easily solved with an interpretation "engine".
+             */
+
             string tokenString = "+ - 10 2 3";
-            List<string> tokenList = new List<string>(tokenString.Split(' '));
+            var tokenList = new List<string>(tokenString.Split(' '));
 
             IExpression expression = new TokenReader().ReadToken(tokenList);
             Console.WriteLine(expression.Interpret());    // (10 - 2) + 3 = 11
