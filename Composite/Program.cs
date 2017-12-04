5a0a0d4383009482e03ad8de1119916121e31468
Hamid Mosalla <Xellarix@gmail.com>
2017-12-04 19:49:43 +0330

    add info to composite pattern main method

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -8,7 +8,16 @@ namespace Composite
     {
         static void Main(string[] args)
         {
+            /*
+            More info: https://en.wikipedia.org/wiki/Composite_pattern
+            What problems can the Composite design pattern solve?
 
+                A part-whole hierarchy should be represented so that clients can treat part and whole objects uniformly.
+                A part-whole hierarchy should be represented as tree structure.
+
+            When defining (1) Part objects and (2) Whole objects that act as containers for Part objects,
+            clients must treat them separately, which complicates client code.
+            */
 
             CompositeGraphic root = new CompositeGraphic("root");
             root.Add(new Leaf("Leaf A"));
