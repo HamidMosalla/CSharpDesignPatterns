5787fdbfa48c321c052b12bc1058146cb7aca067
Hamid Mosalla <Xellarix@gmail.com>
2018-01-06 22:03:21 +0330

    add info for command pattern

--- Command/Client/Program.cs
+++ Command/Client/Program.cs
@@ -9,6 +9,20 @@ namespace Command.Client
     {
         static void Main(string[] args)
         {
+
+            /*
+             More info: https://en.wikipedia.org/wiki/Command_pattern
+             What problems can the Command design pattern solve?
+
+            Coupling the invoker of a request to a particular request should be avoided.
+            That is, hard-wired requests should be avoided.
+            It should be possible to configure an object (that invokes a request) with a request.
+
+            Implementing (hard-wiring) a request directly into a class is inflexible because
+            it couples the class to a particular request at compile-time,
+            which makes it impossible to specify a request at run-time.
+             */
+
             string argument = Console.ReadLine();
 
             ISwitchable lamp = new Light();
