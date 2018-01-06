d7431befbbf893099764780e7bf2d495fcd073fd
Hamid Mosalla <Xellarix@gmail.com>
2018-01-06 21:31:14 +0330

    add more info for adapter pattern

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -8,6 +8,19 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
+
+            /*
+             * The Adapter design pattern solves problems like:
+                How can a class be reused that does not have an interface that a client requires?
+                How can classes that have incompatible interfaces work together?
+                How can an alternative interface be provided for a class?
+
+                Often an (already existing) class can't be reused only 
+                because its interface doesn't conform to the interface clients require.
+             More info: https://csharpdesignpatterns.codeplex.com/wikipage?title=Adapter%20Pattern&referringTitle=Home
+            */
+
+
             var tradingdataImporter = new TradingDataImporter();
 
             Connector databaseConnector = new DatabaseConnector();
